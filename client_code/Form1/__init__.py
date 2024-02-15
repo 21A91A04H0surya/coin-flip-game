c30869a1199aaedb903e67dc4a924ec6784537aa
suryathota11@gmail.com <suryathota11@gmail.com>
2024-02-15 22:06:07 +0000

    Created download.jpg and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,3 +1,5 @@
+import random
+import time
 from ._anvil_designer import Form1Template
 from anvil import *
 
@@ -5,5 +7,23 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    alert("Choose Head or Tails")
 
     # Any code you write here will run before the form opens.
+  def choose_change(self, **event_args):
+    my_change=self.choose_change.selected_value
+  
+  def button_1_click(self, **event_args):
+  
+    self.image_1.source = None
+    coins = ['http://re-bol.com/heads.jpg','http://re-bol.com/tails.jpg'] 
+    coin = random.choice(coins)
+    time.sleep(1)
+    self.image_1.source = URLMedia(coin)
+    time.sleep(1)
+    if coin==coins[0] and self.choose_change.selected_value=='Heads':   
+      alert("You Won")
+    elif coin==coins[1] and self.choose_change.selected_value=='Tails':
+      alert("You Won")
+    else:
+      alert("You Lost")
